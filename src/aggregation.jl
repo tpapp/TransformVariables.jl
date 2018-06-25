8c4eedb7c811889442f915b6b045cb3f67716bdf
Tamas K. Papp <tkpapp@gmail.com>
2018-06-25 11:57:56 +0200

    Added transformations to named tuples.

--- src/aggregation.jl
+++ src/aggregation.jl
@@ -33,34 +33,66 @@ end
 
 # tuples
 
-struct TransformationTuple{T <: Tuple{Vararg{TransformReals}}} <: TransformReals
+const NTransformations{N} = Tuple{Vararg{TransformReals,N}}
+
+struct TransformationTuple{K, T <: NTransformations{K}} <: TransformReals
     transformations::T
-    total_dimension::Int
-    function TransformationTuple(transformations::T) where {T <: Tuple{Vararg{TransformReals}}}
-        new{T}(transformations, sum(dimension, transformations))
+    dimension::Int
+    function TransformationTuple(transformations::T) where {K, T <: NTransformations{K}}
+        new{K,T}(transformations, sum(dimension, transformations))
     end
 end
 
-dimension(transformation_tuple::TransformationTuple) =
-    transformation_tuple.total_dimension
+dimension(tt::TransformationTuple) = tt.dimension
 
-to_tuple(transformations::Tuple{Vararg{TransformReals}}) =
-    TransformationTuple(transformations)
+to_tuple(transformations::NTransformations) = TransformationTuple(transformations)
 
 to_tuple(transformations::TransformReals...) = to_tuple(transformations)
 
-function transform_at(transformation_tuple::TransformationTuple, flag::LogJacFlag,
-                      x::RealVector, index::Int)
-    @unpack transformations = transformation_tuple
+@inline function _transform_tuple(tt::NTransformations, flag::LogJacFlag,
+                                  x::RealVector, index::Int)
     yℓ = map(t -> begin
              result = transform_at(t, flag, x, index)
              index += dimension(t)
              result
-             end, transformations)
+             end, tt)
     first.(yℓ), sum(last, yℓ)
 end
 
-function inverse(t::TransformationTuple{<:Tuple{Vararg{<:TransformReals,K}}},
-                 y::Tuple{Vararg{<:Any,K}}) where K
-    mapreduce(ty -> inverse(ty...), vcat, zip(t.transformations, y))
+@inline _inverse_tuple(tt::NTransformations{K}, y::NTuple{K,Any}) where K =
+    mapreduce(ty -> inverse(ty...), vcat, zip(tt, y))
+
+transform_at(tt::TransformationTuple, flag::LogJacFlag, x::RealVector,
+             index::Int) = _transform_tuple(tt.transformations, flag, x, index)
+
+inverse(tt::TransformationTuple{K}, y::NTuple{K,Any}) where K =
+    _inverse_tuple(tt.transformations, y)
+
+if VERSION ≥ v"0.7-"
+    struct TransformationNamedTuple{names, T <: NTransformations} <: TransformReals
+        transformations::T
+        dimension::Int
+        function TransformationNamedTuple(transformations::NamedTuple{names,T}) where
+            {names, T <: NTransformations}
+            new{names,T}(values(transformations), sum(dimension, transformations))
+        end
+    end
+
+    to_tuple(transformations::NamedTuple{_,<:NTransformations}) where _ =
+        TransformationNamedTuple(transformations)
+
+    to_tuple(; transformations::NTransformations...) =
+        to_tuple(collect(transformations))
+
+    dimension(tn::TransformationNamedTuple) = tn.dimension
+
+    function transform_at(tt::TransformationNamedTuple{names},
+                          flag::LogJacFlag, x::RealVector,
+                          index::Int) where {names}
+        y, ℓ = _transform_tuple(tt.transformations, flag, x, index)
+        NamedTuple{names}(y), ℓ
+    end
+
+    inverse(tn::TransformationNamedTuple{names}, y::NamedTuple{names}) where names =
+        _inverse_tuple(tn.transformations, values(y))
 end
