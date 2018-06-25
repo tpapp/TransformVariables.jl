105a8498a010276a7030368adeeb74c21c39c0c6
Tamas K. Papp <tkpapp@gmail.com>
2018-06-25 13:29:45 +0200

    Made named tuple tests conditional on v0.7.

--- test/runtests.jl
+++ test/runtests.jl
@@ -124,28 +124,30 @@ end
     @test lj2 ≈ ljacc
 end
 
-@testset "to named tuple" begin
-    t1 = to_ℝ
-    t2 = to_𝕀
-    t3 = to_corr_cholesky(7)
-    tn = to_tuple((a = t1, b = t2, c = t3))
-    @test dimension(tn) == dimension(t1) + dimension(t2) + dimension(t3)
-    x = randn(dimension(tn))
-    y = transform(tn, x)
-    @test y isa NamedTuple{(:a,:b,:c)}
-    @test inverse(tn, y) ≈ x
-    index = 0
-    ljacc = 0.0
-    for (i, t) in enumerate((t1, t2, t3))
-        d = dimension(t)
-        xpart = x[index .+ (1:d)]
-        @test y[i] == transform(t, xpart)
-        ypart, ljpart = transform_and_logjac(t, xpart)
-        @test ypart == y[i]
-        ljacc += ljpart
-        index += d
+if VERSION ≥ v"0.7-"
+    @testset "to named tuple" begin
+        t1 = to_ℝ
+        t2 = to_𝕀
+        t3 = to_corr_cholesky(7)
+        tn = to_tuple((a = t1, b = t2, c = t3))
+        @test dimension(tn) == dimension(t1) + dimension(t2) + dimension(t3)
+        x = randn(dimension(tn))
+        y = transform(tn, x)
+        @test y isa NamedTuple{(:a,:b,:c)}
+        @test inverse(tn, y) ≈ x
+        index = 0
+        ljacc = 0.0
+        for (i, t) in enumerate((t1, t2, t3))
+            d = dimension(t)
+            xpart = x[index .+ (1:d)]
+            @test y[i] == transform(t, xpart)
+            ypart, ljpart = transform_and_logjac(t, xpart)
+            @test ypart == y[i]
+            ljacc += ljpart
+            index += d
+        end
+        y2, lj2 = transform_and_logjac(tn, x)
+        @test y == y2
+        @test lj2 ≈ ljacc
     end
-    y2, lj2 = transform_and_logjac(tn, x)
-    @test y == y2
-    @test lj2 ≈ ljacc
 end
