d9823ba89d732e25c0f135f6d37735d3fe2b6ddd
Tamas K. Papp <tkpapp@gmail.com>
2018-08-29 15:48:42 +0200

    fix missing comma

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(;
 
 deploydocs(
     repo = "github.com/tpapp/TransformVariables.jl.git",
-    julia = "1.0"
+    julia = "1.0",
     target = "build",
     deps = nothing,
     make = nothing
