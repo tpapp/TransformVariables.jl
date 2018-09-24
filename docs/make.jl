a2e23345edb04a4ad51cb33f063bd714923a16c0
Tamas K. Papp <tkpapp@gmail.com>
2018-09-24 10:35:31 +0200

    reorganize testing and documentation deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,13 @@
-using Documenter, TransformVariables
+makedocs(
+    modules = [TransformVariables],
+    format = :html,
+    sitename = "$TransformVariables.jl",
+    pages = Any["index.md"]
+)
 
-makedocs(;
-         sitename = "TransformVariables.jl",
-         modules = [TransformVariables],
-         format = :html,
-         clean = true,
-         checkdocs = :exports,
-         pages = Any["Manual" => "index.md"]
+deploydocs(
+    repo = "github.com/tpapp/TransformVariables.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
 )
