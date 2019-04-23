d7ebdf30a699726f8f20e73a9a54e3dbdec943aa
Tamas K. Papp <tkpapp@gmail.com>
2019-04-23 08:41:00 +0200

    Package and CI cleanup.
    Remove REQUIRE and obsolete scripts, test on 1.1.

--- submit_coverage.jl
+++
@@ -1,7 +0,0 @@
-using Pkg
-pkg"add Coverage"
-using Coverage
-# push coverage results to Coveralls
-Coveralls.submit(Coveralls.process_folder())
-# push coverage results to Codecov
-Codecov.submit(Codecov.process_folder())
