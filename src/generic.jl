b01d9dceb159b06575b880b232a6f63ad37a8446
Takafumi Arakaki <aka.tkf@gmail.com>
2019-10-04 22:10:06 -0700

    Mention "vectorization" of transforms

--- src/generic.jl
+++ src/generic.jl
@@ -189,6 +189,7 @@ See `methods(as)` for a list.
 ```julia
 as(Real, -∞, 1)          # transform a real number to (-∞, 1)
 as(Array, 10, 2)         # reshape 20 real numbers to a 10x2 matrix
+as(Array, as𝕀, 10)       # transform 10 real numbers to (0, 1)
 as((a = asℝ₊, b = as𝕀)) # transform 2 real numbers a NamedTuple, with a > 0, 0 < b < 1
 ```
 """
