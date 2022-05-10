18183e0d164ee3da3eac8744f1e7a6b798dd9750
Chris Elrod <elrodc@gmail.com>
2022-05-10 15:30:57 -0400

    no zip in _inverse_eltype_tuple

--- test/runtests.jl
+++ test/runtests.jl
@@ -218,7 +218,7 @@ end
     x = random_arg(tt)
     y = @inferred transform(tt, x)
     @test inverse(tt, y) ≈ x
-    TransformVariables.inverse_eltype(tt, y)
+    @test @inferred(TransformVariables.inverse_eltype(tt, y)) === Float64
     index = 0
     ljacc = 0.0
     for (i, t) in enumerate((t1, t2, t3))
@@ -537,6 +537,15 @@ end
     shifted = TransformVariables.ShiftedExp{true,Float64}(0.0)
     tr = (a = shifted, b = TransformVariables.Identity(), c = shifted, d = shifted, e = shifted, f = shifted)
     @test iszero(@allocated TransformVariables._sum_dimensions(tr))
+end
+
+@testset "inverse_eltype allocations" begin
+  trf = as((x0 = TransformVariables.ShiftedExp{true, Float32}(0f0), x1 = TransformVariables.Identity(), x2 = UnitSimplex(7), x3 = TransformVariables.CorrCholeskyFactor(5), x4 = as(Real, -∞, 1), x5 = as(Array, 10, 2), x6 = as(Array, as𝕀, 10), x7 = as((a = asℝ₊, b = as𝕀)), x8 = TransformVariables.UnitVector(10), x9 = TransformVariables.ShiftedExp{true, Float32}(0f0), x10 = TransformVariables.ShiftedExp{true, Float32}(0f0), x11 = TransformVariables.ShiftedExp{true, Float32}(0f0), x12 = TransformVariables.ShiftedExp{true, Float32}(0f0), x13 = TransformVariables.Identity(), x14 = TransformVariables.ShiftedExp{true, Float32}(0f0), x15 = TransformVariables.ShiftedExp{true, Float32}(0f0), x16 = TransformVariables.ShiftedExp{true, Float32}(0f0), x17 = TransformVariables.ShiftedExp{true, Float64}(0.0)));
+  
+  vx = randn(@inferred(TransformVariables.dimension(trf)));
+  x = TransformVariables.transform(trf, vx);
+  @test @inferred(TransformVariables.inverse_eltype(trf, x)) === Float64
+  
 end
 
 ####
