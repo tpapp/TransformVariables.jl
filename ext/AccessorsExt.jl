8148e51545a9513e0f31d7ffd8fb7ac5c1ba9699
Tamás K. Papp <tkpapp@gmail.com>
2025-10-08 13:35:08 +0200

    actually commit the extension

---
+++ ext/AccessorsExt.jl
@@ -0,0 +1,17 @@
+module AccessorsExt
+
+import Accessors: set, insert, delete
+using Accessors: IndexLens, PropertyLens
+using TransformVariables: TransformTuple, _inner
+
+const LENSES = Union{IndexLens,PropertyLens}
+
+set(t::TransformTuple, ::typeof(_inner), inner) = TransformTuple(inner)
+set(t::TransformTuple, lens::IndexLens, val) = set(t, lens ∘ _inner, val)
+insert(t::TransformTuple{<:Tuple}, lens::IndexLens, val) = insert(t, lens ∘ _inner, val)
+delete(t::TransformTuple, lens::IndexLens) = delete(t, lens ∘ _inner)
+set(t::TransformTuple{<:NamedTuple}, lens::PropertyLens, val) = set(t, lens ∘ _inner, val)
+insert(t::TransformTuple{<:NamedTuple}, lens::PropertyLens, val) = insert(t, lens ∘ _inner, val)
+delete(t::TransformTuple{<:NamedTuple}, lens::PropertyLens) = delete(t, lens ∘ _inner)
+
+end
