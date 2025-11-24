e9ac0f02f7cd4e6123451f7ed820b26c38cca763
Tamas K. Papp <tkpapp@gmail.com>
2025-11-24 17:26:09 +0100

    Update src/utilities.jl
    Co-authored-by: David Müller-Widmann <devmotion@users.noreply.github.com>

--- src/utilities.jl
+++ src/utilities.jl
@@ -100,6 +100,6 @@ function _check_name_compatibility(::Type{<:NamedTuple{A}},
     for b in B
         b ∈ A || throw(ArgumentError("Property :$(b) not in $(A)."))
     end
-    length(A) == length(B) || throw(ArgumentError("Names $(A) has extras compared to $(B)."))
+    length(A) == length(B) || throw(ArgumentError(LazyString("Names ", A, " has extras compared to ", B, ".")))
     nothing
 end
